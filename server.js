875e765df986df5ffee4397c8d1a318b416e0d38
Gustavo <gustavo@gmail.com>
2022-11-11 13:02:31 -0500

    second commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.get("/water", (req, res) => {
+    console.log("Express here!!!! Welcome to a new Railway")
+    res.send("Here is some water for you")
+})
+
+app.listen(port, () => {
+    console.log(`Our app is running on port ${ port }`);
+});
