d357a4f45526f7a790b55720f4ab4b4d56034d04
Gustavo <gustavo@gmail.com>
2022-11-14 09:58:29 -0500

    ok monday

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import express from "express";
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.get("/water", (req, res) => {
-    console.log("Express here!!!! Welcome to a new Railway yee on master again again ")
-    res.send("Here is some water for you")
+app.get("/", function(req, res){
+    res.send("Express here!!!! Here I'am 2 :) - But 23-new 2 - LOCALLLLL")
+    console.log("Express here!!!! Here I'am 2 :)")
 })
+
 
 app.listen(port, () => {
     console.log(`Our app is running on port ${ port }`);
