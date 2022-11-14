50fe8aba841a315b60c1634a7fe5d646b58b7d6e
Gustavo <gustavo@gmail.com>
2022-11-14 14:25:49 -0500

    oki 2323

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Our app is running on portss ${ PORT }`);
+    //console.log(`Our app is running on portss ${ PORT }`);
 });
 
 app.get("/", function(req, res){
-    res.send("Express here!!!! Here I'am 2 :) - But 23-new 2 - LOCALLLLL - ahora estamos en v.002")
-    console.log("Express here!!!! Here I'am 2 :)")
+    res.send("Welcome to Principal - Running on Vercel")
+    //console.log("Express here!!!! Here I'am 2 :)")
 })
 
 
