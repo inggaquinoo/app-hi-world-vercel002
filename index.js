68df7b1013a2d2d704046204dc885ba45a273588
Gustavo <gustavo@gmail.com>
2022-11-14 13:40:35 -0500

    ok change names

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
+app.get("/", function(req, res){
+    res.send("Express here!!!! Here I'am 2 :) - But 23-new 2 - LOCALLLLL - ahora estamos en v.002")
+    console.log("Express here!!!! Here I'am 2 :)")
+})
+
+
+
+//app.use("/users/", require("./routes/usersRoute"));
+
+
+
