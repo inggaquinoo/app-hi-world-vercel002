0a9463683f4523e96faa466619f28cff6ac5e161
Gustavo <gustavo@gmail.com>
2022-11-14 10:00:58 -0500

    ok monday 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.get("/", function(req, res){
+app.get("/hola", function(req, res){
     res.send("Express here!!!! Here I'am 2 :) - But 23-new 2 - LOCALLLLL")
     console.log("Express here!!!! Here I'am 2 :)")
 })
