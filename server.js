62eccb41f7938e9f78ec4cbdd60635f173e391c9
Gustavo <gustavo@gmail.com>
2022-11-14 10:20:36 -0500

    update .env

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
 
-app.get("/hola", function(req, res){
+
+app.get("/", function(req, res){
     res.send("Express here!!!! Here I'am 2 :) - But 23-new 2 - LOCALLLLL")
     console.log("Express here!!!! Here I'am 2 :)")
 })
 
 
-app.listen(port, () => {
-    console.log(`Our app is running on port ${ port }`);
+
+//app.use("/users/", require("./routes/usersRoute"));
+
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
